42352f3b47ab8a6e0dd5c6c39d28bdf0c591f4ab
LXY <2227697636@qq.com>
2019-12-21 16:50:11 +0800

    complete Info and modify Info

---
+++ src/plugins/element.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI);
+// Vue.use(ElementUI, { size: 'small', zIndex: 3000 })
+// （3）在引入 Element 时，可以传入一个全局配置对象。该对象目前支持 size 与 zIndex 字段。size 用于改变组件的默认尺寸，zIndex 设置弹框的初始 z-index（默认值：2000）
