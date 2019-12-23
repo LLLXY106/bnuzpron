d292ba42289e9094f7b934b59884768fe1df0e22
LXY <2227697636@qq.com>
2019-12-23 21:53:01 +0800

    优化了排行榜 给前三加图标 其余的自动从4开始排

--- src/views/RankingList.vue
+++ src/views/RankingList.vue
@@ -2,7 +2,9 @@
     <div class="bg">
         <TopContainer></TopContainer>
         <div class="hd">
-            <div class="dim"><p STYLE="font-size:44px;font-weight: bolder;line-height: 100px;letter-spacing:10px;">排行榜</p><p STYLE="font-size:16px;letter-spacing:4px;">RANKINGLIST</p></div>
+            <div class="dim"><p STYLE="font-size:44px;font-weight: bolder;line-height: 100px;letter-spacing:10px;">
+                排行榜</p>
+                <p STYLE="font-size:16px;letter-spacing:4px;">RANKINGLIST</p></div>
         </div>
         <div class="rank_body">
             <div class="rank_head">
@@ -10,28 +12,41 @@
                 <p style="line-height: 32px">热门视频排行榜</p>
             </div>
             <div class="item">
-                <div class="num" style="width: 6%;height: 100%;text-align: center"><img src="../assets/images/gold.png"/></div>
+                <div class="num" style="width: 6%;height: 100%;text-align: center"><img
+                        src="../assets/images/gold.png"/></div>
                 <img :src="Video[0].headsrc" style="width: 188px;height: 119px;margin-right: 20px"/>
-                视频标题：{{Video[0].title}}
-                总播放次数:{{Video[0].play}}
+                <div class="detail">
+                    <div style="margin-bottom:30px">视频标题：{{Video[0].title}}</div>
+                    <div style="color: #fa5a57">总播放次数:{{Video[0].play}}</div>
+                </div>
             </div>
             <div class="item">
-                <div class="num" style="width: 6%;height: 100%;text-align: center"><img src="../assets/images/silver.png"/></div>
+                <div class="num" style="width: 6%;height: 100%;text-align: center"><img
+                        src="../assets/images/silver.png"/></div>
                 <img :src="Video[1].headsrc" style="width: 188px;height: 119px;margin-right: 20px"/>
-                视频标题：{{Video[1].title}}
-                总播放次数:{{Video[1].play}}
+                <div class="detail">
+                    <div style="margin-bottom:30px">视频标题：{{Video[1].title}}</div>
+                    <div style="color: #409EFF">总播放次数:{{Video[1].play}}</div>
+                </div>
             </div>
             <div class="item">
-                <div class="num" style="width: 6%;height: 100%;text-align: center"><img src="../assets/images/copper.png"/></div>
+                <div class="num" style="width: 6%;height: 100%;text-align: center"><img
+                        src="../assets/images/copper.png"/></div>
                 <img :src="Video[2].headsrc" style="width: 188px;height: 119px;margin-right: 20px"/>
-                视频标题：{{Video[2].title}}
-                总播放次数:{{Video[2].play}}
+                <div class="detail">
+                    <div style="margin-bottom:30px">视频标题：{{Video[2].title}}</div>
+                    <div style="color: #409EFF">总播放次数:{{Video[2].play}}</div>
+                </div>
             </div>
             <div class="item" v-for="(items,i) in sortVideo" v-if="i >= 3">
-                <div class="num" style="width: 6%;height: 100%;text-align: center;font-size: 16px;font-weight: bold">{{i+1}}</div>
+                <div class="num" style="width: 6%;height: 100%;text-align: center;font-size: 24px;font-weight: bold">
+                    {{i+1}}
+                </div>
                 <img :src="items.headsrc" style="width: 188px;height: 119px;margin-right: 20px"/>
-                视频标题：{{items.title}}
-                总播放次数:{{items.play}}
+                <div class="detail">
+                    <div style="margin-bottom:30px">视频标题：{{items.title}}</div>
+                    <div style="color: #409EFF">总播放次数:{{items.play}}</div>
+                </div>
             </div>
         </div>
     </div>
@@ -85,22 +100,24 @@
 </script>
 
 <style lang="stylus" scoped>
-    .hd{
+    .hd {
         width 100%
         height 160px
         position: relative;
-        background:url("../assets/images/r_bg.jpg") no-repeat top
+        background: url("../assets/images/r_bg.jpg") no-repeat top
 
     }
-    .dim{
+
+    .dim {
         width 100%
         height 160px
-        background-color: rgba(0,0,0,.6);
+        background-color: rgba(0, 0, 0, .6);
         color white
         font-family "微软雅黑", sans-serif
         text-align center
     }
-    .item{
+
+    .item {
         /*border 1px red solid*/
         width 100%
         height 150px
@@ -111,21 +128,25 @@
         display flex
         flex-direction row
     }
+
     .item:hover {
-        box-shadow:0px 2px 3px 2px rgba(205,205,205,0.64);
-        transform:scale(1.01);
+        box-shadow: 0px 2px 3px 2px rgba(205, 205, 205, 0.64);
+        transform: scale(1.01);
         /*background-color #fffcff*/
-        transform:translateX(0);
+        transform: translateX(0);
 
     }
+
     .item:hover:before {
-        opacity:0.5;
+        opacity: 0.5;
     }
-    .item:before{
-        opacity:0;
-        transition:all 500ms cubic-bezier(0.47,0,0.745,0.715) 0s;
+
+    .item:before {
+        opacity: 0;
+        transition: all 500ms cubic-bezier(0.47, 0, 0.745, 0.715) 0s;
     }
-    .rank_head{
+
+    .rank_head {
         font-size 14px
         /*border 1px red solid*/
         text-align left
@@ -134,9 +155,14 @@
         padding-bottom 5px
         border-bottom 1px #979797 solid
     }
-    .rank_body{
+
+    .rank_body {
         margin 20px auto
         /*border 1px red solid*/
         width 80%
+    }
+    .detail{
+        display flex
+        flex-direction column
     }
 </style>
